ef13d64d98b2a0f58da0909d29a0f19e9ad73f2b
abouyamourn <32350564+abouyamourn@users.noreply.github.com>
2018-10-29 17:59:43 -0700

    Add files via upload

--- scrapers/42091.R
+++ scrapers/42091.R
@@ -2,21 +2,34 @@ library(ygresults)
 library(stringr)
 library(dplyr)
 
+options(parallel_packing = F)
+
 ELECTION_CODE <- "test_20181029"
 COUNTY_CODE <- "42091"
+
+schema_precincts(COUNTY_CODE)
+
 
 raw <- raw_download(ELECTION_CODE, COUNTY_CODE) %>%
     mutate(district = as.numeric(district), state = as.numeric(state), county = as.numeric(county),
            votes = as.integer(votes))
 
+head(raw)
+
 ## precinct names
 df <- raw %>%
     mutate(precinct = as.character(precinct))
 
+df$precinct <- str_replace_all(df$precinct, "-[0-9][0-9][0-9][0-9]", "")
+
+tail(df, 100)
+
+df <- df %>% mutate(precinct = as.character(precinct))
+
 ## candidate names
 df <- df %>%
-    mutate(candidate = str_replace_all(candidate, " (I+|Jr\\.*|Sr\\.*)$", "") %>%
-               str_extract("((De|La) )*(\\w|-|')+$") %>%
+    mutate(candidate = str_replace_all(candidate, "(I+|, JR\\.*|, SR\\.*)$", "") %>%
+               str_extract("((De|La) )*(\\w|-|'|,)+$") %>%
                capitalize_names()
     )
 
@@ -27,6 +40,10 @@ df <- df %>%
 out <- df %>% select(precinct, district, office, candidate, party, votetype, votes)
 
 out %>% results_schema(COUNTY_CODE)
+
+out
+
+unique(out$precinct)
 
 out %>% results_upload(election_code = ELECTION_CODE,
                        county_code = COUNTY_CODE)
